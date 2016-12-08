377f64d5378639d3f6d7f6bf391fcc0db71344b1
vinamarora8 <vinam.arora8@gmail.com>
2016-12-08 09:52:04 +0530

    Phase 2 implemented

---
+++ README.md
@@ -0,0 +1,11 @@
+# Terminal Colors handler for Python
+
+Makes the world a better place by easing the process of making a colored terminal program
+
+### Usage Instruction :
++ Import the library - `from pyterm_colors import *
++ Create instance of color class - `c = color()
++ set color using 2 methods:
++ + 1) `c.setc("blue")
++ + 2) `print c.blue
++ In either case, all the following lines will be blue colored untill another such statement is called
---
+++ pyterm_colors.py
@@ -0,0 +1,43 @@
+#global default = "hey"
+
+class color:
+#	
+# Part one: To be used as :
+# from pylinux-colors import * 
+# print color.red + "your-text"
+#
+	default = '\033[39m'
+	black = '\033[30m'
+	red = '\033[31m'
+	green = '\033[32m'
+	yellow = '\033[33m'
+	blue = '\033[34m'
+	magenta = '\033[35m'
+	cyan = '\033[36m'
+	white = '\033[00m'
+#
+# Part two: To be used as :
+# from pylinux-colors import *
+# color.set(green)
+#
+	def setc(self, colo):
+		if colo == "default":
+			print self.default
+		elif colo == "black":
+			print self.black
+		elif colo == "red":
+			print self.red
+		elif colo == "green":
+			print self.green
+		elif colo == "yellow":
+			print self.yellow
+		elif colo == "blue":
+			print self.blue
+		elif colo == "magenta":
+			print self.magenta
+		elif colo == "cyan":
+			print self.cyan
+		elif colo == "white":
+			print self.white
+		else:
+			print self.red
